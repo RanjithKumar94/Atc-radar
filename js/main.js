4e61f5fe74051d3a56ac9f75feca686f8aebf905
RanjithKumar94 <ranjithkumar3593@gmail.com>
2026-07-18 19:14:50 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
+function moveAircraft() {
 
-window.onload = function () {
+    aircraft.forEach(ac => {
 
-    console.log("ATC Radar Simulator Started");
+        // Turn towards assigned heading
+        let diff = (ac.assignedHeading - ac.heading + 360) % 360;
 
-    if (typeof drawRadar === "function") {
-        drawRadar();
-    }
+        if(diff > 180){
+            ac.heading -= 1;
+        }else if(diff > 0){
+            ac.heading += 1;
+        }
 
-};
+        if(ac.heading < 0) ac.heading += 360;
+        if(ac.heading >= 360) ac.heading -= 360;
+
+        // Move
+        let rad = ac.heading * Math.PI / 180;
+
+        ac.x += Math.sin(rad) * 0.7;
+        ac.y -= Math.cos(rad) * 0.7;
+
+        // Climb / Descend
+        if(ac.level > ac.assignedLevel){
+            ac.level -= 1;
+        }
+
+        if(ac.level < ac.assignedLevel){
+            ac.level += 1;
+        }
+
+    });
+
+}
+
+setInterval(moveAircraft,100);
